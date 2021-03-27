a7bca01e125195e39de14d98b24e6a3f63637e36
DimaShustal <dzmitry.shustal@gmail.com>
2021-03-27 17:38:06 +0300

    [3.1] LimitSizeStream was implemented

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,23 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #maxStreamSize = 0;
+  #currentStreamSize = 0;
+
   constructor(options) {
     super(options);
+
+    this.#maxStreamSize = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    this.#currentStreamSize += chunk.length;
 
+    if (this.#maxStreamSize < this.#currentStreamSize) {
+      callback(new LimitExceededError());
+    } else {
+      callback(undefined, chunk);
+    }
   }
 }
 
