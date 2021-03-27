df1a318130a69679b135b597d426bedb860cbff0
DimaShustal <dzmitry.shustal@gmail.com>
2021-03-27 18:59:19 +0300

    [3.2] LineSplitStream was implemented

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,23 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  #prevChunkAsString = '';
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    const chunkAsString = this.#prevChunkAsString + chunk.toString();
+    const chunkAsArray = chunkAsString.split(os.EOL);
+
+    this.#prevChunkAsString = chunkAsArray.pop();
+    chunkAsArray.forEach(this.push.bind(this));
+    callback();
   }
 
   _flush(callback) {
+    callback(undefined, this.#prevChunkAsString);
   }
 }
 
