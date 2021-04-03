ade1e5b177ec50118a0479b7e5272419d1e3b66e
DimaShustal <dzmitry.shustal@gmail.com>
2021-04-03 22:32:51 +0300

    [5.3] server that delete files was implemented

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,22 +1,48 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs/promises');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'DELETE':
+    case 'DELETE': {
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Subfolders are not supported');
+        return;
+      }
+
+      try {
+        await fsPromises.access(filepath, fs.constants.F_OK);
+        await fsPromises.unlink(filepath);
+        res.statusCode = 200;
+        res.end('Success');
+        return;
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+          return;
+        }
+
+        res.statusCode = 500;
+        res.end('Something went wrong');
+        return;
+      }
 
       break;
+    }
 
-    default:
+    default: {
       res.statusCode = 501;
       res.end('Not implemented');
+    }
   }
 });
 
