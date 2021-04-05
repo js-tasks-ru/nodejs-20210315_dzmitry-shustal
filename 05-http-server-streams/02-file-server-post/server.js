d7064e17154f16d84089a9ca2ed81378accd731e
DimaShustal <dzmitry.shustal@gmail.com>
2021-04-05 21:44:00 +0300

    [5.2] server that saves files was changed

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs/promises');
 const stream = require('stream');
-const util = require('util');
+// const util = require('util');
 const LimitSizeStream = require('./LimitSizeStream');
 
-const pipeline = util.promisify(stream.pipeline);
+// const pipeline = util.promisify(stream.pipeline);
 const server = new http.Server();
 
 server.on('request', async (req, res) => {
@@ -28,57 +28,56 @@ server.on('request', async (req, res) => {
       const limitedStream = new LimitSizeStream({limit: 1000000});
 
       // OPTION 1 - pass tests
-      // writeStream.on('error', (err) => {
-      //   res.statusCode = 409;
-      //   res.end('The file already exists');
-      //   return;
-      // });
-      //
-      // limitedStream.on('error', (err) => {
-      //   fsPromises.unlink(filepath);
-      //   res.statusCode = 413;
-      //   res.end('File size limit is 1 mb');
-      //   return;
-      // });
-      //
-      // pipeline(
-      //   req,
-      //   limitedStream,
-      //   writeStream,
-      //   (err, data) => {
-      //     if (!err) {
-      //       res.statusCode = 201;
-      //       res.end();
-      //     }
-      //   },
-      // );
+      writeStream.on('error', (err) => {
+        res.statusCode = 409;
+        res.end('The file already exists');
+        return;
+      });
 
+      limitedStream.on('error', (err) => {
+        res.statusCode = 413;
+        res.end('File size limit is 1 mb');
+        fsPromises.unlink(filepath);
+        return;
+      });
+
+      stream.pipeline(
+        req,
+        limitedStream,
+        writeStream,
+        (err, data) => {
+          if (!err) {
+            res.statusCode = 201;
+            res.end();
+          }
+        },
+      );
 
       // OPTION 2 - doesn't pass tests (при попытке создания слишком большого файла - ошибка 413)
-      // what is wrong?
-      try {
-        await pipeline(req, limitedStream, writeStream);
-        res.statusCode = 201;
-        res.end();
-      } catch (err) {
-        if (err.code === 'LIMIT_EXCEEDED') {
-          res.statusCode = 413;
-          res.end('File size limit is 1 mb');
-          await fsPromises.unlink(filepath);
-          return;
-        }
-
-        if (err.code === 'EEXIST') {
-          res.statusCode = 409;
-          res.end('The file already exists');
-          return;
-        }
-
-        res.statusCode = 500;
-        res.end('Something went wrong');
-        await fsPromises.unlink(filepath);
-        return;
-      }
+      // Reason: https://github.com/nodejs/node/issues/31630
+      // try {
+      //   await pipeline(req, limitedStream, writeStream);
+      //   res.statusCode = 201;
+      //   res.end();
+      // } catch (err) {
+      //   if (err.code === 'LIMIT_EXCEEDED') {
+      //     res.statusCode = 413;
+      //     res.end('File size limit is 1 mb');
+      //     await fsPromises.unlink(filepath);
+      //     return;
+      //   }
+      //
+      //   if (err.code === 'EEXIST') {
+      //     res.statusCode = 409;
+      //     res.end('The file already exists');
+      //     return;
+      //   }
+      //
+      //   res.statusCode = 500;
+      //   res.end('Something went wrong');
+      //   await fsPromises.unlink(filepath);
+      //   return;
+      // }
 
       break;
     }
