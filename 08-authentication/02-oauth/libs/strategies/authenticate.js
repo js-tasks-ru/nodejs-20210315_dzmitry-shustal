91751005dc3d0e6bf383a55baa00e1b434e47530
DimaShustal <dzmitry.shustal@gmail.com>
2021-04-19 08:28:46 +0300

    [8.2] return operator was added to authenticate.js
    this is necessary to stop the function

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -3,19 +3,21 @@ const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
   try {
-    if (!email) done(null, false, `Не указан email`);
+    if (!email) {
+      return done(null, false, `Не указан email`);
+    }
 
     const user = await User.findOne({email});
 
     if (user) {
-      done(null, user);
-    } else {
-      const newUser = new User({email, displayName});
-
-      await newUser.setPassword(uuid());
-      await newUser.save();
-      done(null, newUser);
+      return done(null, user);
     }
+
+    const newUser = new User({email, displayName});
+
+    await newUser.setPassword(uuid());
+    await newUser.save();
+    return done(null, newUser);
   } catch (e) {
     done(e);
   }
