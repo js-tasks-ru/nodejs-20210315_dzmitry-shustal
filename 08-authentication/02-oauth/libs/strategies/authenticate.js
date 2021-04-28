287e00b4b375b78b6a4073ce5b47df6836d15e2b
DimaShustal <dzmitry.shustal@gmail.com>
2021-04-28 19:02:52 +0300

    [8.2] check on email was moved from try/catch

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -2,11 +2,11 @@ const {v4: uuid} = require('uuid');
 const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
-  try {
-    if (!email) {
-      return done(null, false, `Не указан email`);
-    }
+  if (!email) {
+    return done(null, false, `Не указан email`);
+  }
 
+  try {
     const user = await User.findOne({email});
 
     if (user) {
